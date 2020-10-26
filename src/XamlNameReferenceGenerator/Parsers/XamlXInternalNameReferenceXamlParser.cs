8e99bc32c71fbdd5563548231298bf90a942197c
Artyom <worldbeater-dev@yandex.ru>
2020-10-26 18:00:53 +0300

    Add XamlX etc.

---
+++ src/XamlNameReferenceGenerator/Parsers/XamlXInternalNameReferenceXamlParser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace XamlNameReferenceGenerator.Parsers
+{
+    public class XamlXInternalNameReferenceXamlParser : INameReferenceXamlParser
+    {
+        public List<(string TypeName, string Name)> GetNamedControls(string xaml)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
