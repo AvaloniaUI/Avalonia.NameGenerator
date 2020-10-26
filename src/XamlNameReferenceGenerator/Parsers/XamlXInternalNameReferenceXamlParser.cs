dcb417c9eb9b47e2179d8bd02aaf968c8c692000
Artyom <worldbeater-dev@yandex.ru>
2020-10-26 19:08:18 +0300

    Add XAML parser based on XamlX

--- src/XamlNameReferenceGenerator/Parsers/XamlXInternalNameReferenceXamlParser.cs
+++ src/XamlNameReferenceGenerator/Parsers/XamlXInternalNameReferenceXamlParser.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using XamlX;
+using XamlX.Ast;
+using XamlX.Parsers;
 
 namespace XamlNameReferenceGenerator.Parsers
 {
@@ -6,7 +9,59 @@ namespace XamlNameReferenceGenerator.Parsers
     {
         public List<(string TypeName, string Name)> GetNamedControls(string xaml)
         {
-            throw new System.NotImplementedException();
+            var parsed = XDocumentXamlParser.Parse(xaml, new Dictionary<string, string>
+            {
+                {XamlNamespaces.Blend2008, XamlNamespaces.Blend2008}
+            });
+
+            var visitor = new XamlAstCollector();
+            parsed.Root.Visit(visitor);
+            parsed.Root.VisitChildren(visitor);
+            return visitor.Controls;
+        }
+
+        private class XamlAstCollector : IXamlAstVisitor
+        {
+            public List<(string TypeName, string Name)> Controls { get; } = new List<(string TypeName, string Name)>();
+
+            public IXamlAstNode Visit(IXamlAstNode node)
+            {
+                if (node is XamlAstObjectNode element && element.Type is XamlAstXmlTypeReference type)
+                {
+                    foreach (var child in element.Children)
+                    {
+                        var nameValue = ResolveNameDirectiveOrDefault(child);
+                        if (nameValue == null) continue;
+
+                        var typeNamePair = (type.Name, nameValue);
+                        if (!Controls.Contains(typeNamePair))
+                            Controls.Add(typeNamePair);
+                    }
+                }
+
+                return node;
+            }
+
+            public void Push(IXamlAstNode node) { }
+
+            public void Pop() { }
+
+            private static string ResolveNameDirectiveOrDefault(IXamlAstNode node) =>
+                node switch
+                {
+                    XamlAstXamlPropertyValueNode propertyValueNode when
+                        propertyValueNode.Property is XamlAstNamePropertyReference reference &&
+                        reference.Name == "Name" &&
+                        propertyValueNode.Values.Count > 0 &&
+                        propertyValueNode.Values[0] is XamlAstTextNode nameNode => nameNode.Text,
+
+                    XamlAstXmlDirective xmlDirective when
+                        xmlDirective.Name == "Name" &&
+                        xmlDirective.Values.Count > 0 &&
+                        xmlDirective.Values[0] is XamlAstTextNode xNameNode => xNameNode.Text,
+
+                    _ => null
+                };
         }
     }
 }
