ac9213d8c44eb6b417f7d689d5dbf38d1b5b616e
Artyom <worldbeater-dev@yandex.ru>
2020-10-31 11:12:21 +0300

    What if we add a name to a data template?

--- src/XamlNameReferenceGenerator/Infrastructure/MiniCompiler.cs
+++ src/XamlNameReferenceGenerator/Infrastructure/MiniCompiler.cs
@@ -26,8 +26,8 @@ namespace XamlNameReferenceGenerator.Infrastructure
         private MiniCompiler(TransformerConfiguration configuration)
             : base(configuration, new XamlLanguageEmitMappings<object, IXamlEmitResult>(), false)
         {
-            Transformers.Add(new DataTemplateTransformer());
             Transformers.Add(new NameDirectiveTransformer());
+            Transformers.Add(new DataTemplateTransformer());
             Transformers.Add(new KnownDirectivesTransformer());
             Transformers.Add(new XamlIntrinsicsTransformer());
             Transformers.Add(new XArgumentsTransformer());
